4d933bf39f10fc53ad00112c51891d38f2b4a482
Slava Melman <xdslavaxd@yandex.ru>
2024-06-29 07:36:58 +0300

    fix1

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -1,18 +1,7 @@
-from unittest.mock import Mock
-
-
 class TestBun:
 
-    def test_get_name_bun(self):
-        # Создаем мок-объект булочки
-        mock_bun = Mock()
-        mock_bun.name = "Черная дыра"
+    def test_get_name_bun(self, get_mock_bun):
+        assert get_mock_bun.name == "Черная дыра"
 
-        assert mock_bun.name == "Черная дыра"
-
-    def test_get_price_bun(self):
-        # Создаем мок-объект булочки
-        mock_bun = Mock()
-        mock_bun.price = 888
-
-        assert mock_bun.price == 888
+    def test_get_price_bun(self, get_mock_bun):
+        assert get_mock_bun.price == 888
--- tests/test_ingredient.py
+++ tests/test_ingredient.py
@@ -15,20 +15,18 @@ class TestIngredient:
         assert new_ingredient.get_name() == name
         assert new_ingredient.get_price() == price
 
-    def test_get_price_new_ingredient(self):
-        # Cоздаем новый ингредиент с ценой 66
-        new_ingredient = Ingredient("SAUCE", "No Way", 66)
+    def test_get_price_new_ingredient(self, get_sauce_ingredient):
+        # Тестируем, что цена ингредиента с типом SAUCE равна 66
+        assert get_sauce_ingredient.get_price() == 66
 
-        assert new_ingredient.get_price() == 66
+    def test_get_name_ingredient(self, get_sauce_ingredient):
+        # Тестируем, что название ингредиента с типом SAUCE равно "No Way"
+        assert get_sauce_ingredient.get_name() == "No Way"
 
-    def test_get_name_ingredient(self):
-        # Cоздаем новый ингредиент с названием "No Way'
-        new_ingredient = Ingredient("SAUCE", "No Way", 66)
+    def test_get_type_ingredient(self, get_sauce_ingredient):
+        # Тестируем, что тип ингредиента с типом SAUCE равен "SAUCE"
+        assert get_sauce_ingredient.get_type() == "SAUCE"
 
-        assert new_ingredient.get_name() == "No Way"
-
-    def test_get_type_ingredient(self):
-        # Cоздаем новый ингредиент с типом "SAUCE"
-        new_ingredient = Ingredient("SAUCE", "No Way", 66)
-
-        assert new_ingredient.get_type() == "SAUCE"
+    def test_get_another_ingredient(self, get_filling_ingredient):
+        # Тестируем, что цена ингредиента с типом FILLING равна 99
+        assert get_filling_ingredient.get_price() == 99
